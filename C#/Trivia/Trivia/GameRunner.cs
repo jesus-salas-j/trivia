8ce76daf7667da5b57008ac3b65c22f57e5ab50e
jesus-salas-j <jesus.salas@schibsted.com>
2019-07-16 08:50:33 +0200

    Added seeds

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -20,6 +20,9 @@ namespace Trivia
             aGame.add("Pat");
             aGame.add("Sue");
 
+            List<int> rolls = CreateRollsSequence();
+            List<int> squares = CreateSquaresSequence();
+
             Random rand = new Random();
 
             do
@@ -42,7 +45,23 @@ namespace Trivia
 
         }
 
+        private static List<int> CreateSquaresSequence()
+        {
+            return new List<int>
+            {
+                6,9,2,4,7,0,5,1,0,2,8,4,2,3,8,1,2,5,1,7,8,4,5,7,8,
+                1,3,1,9,2,9,6,2,2,1,0,2,6,1,2,8,9,7,1,3,5,2,5,1,3,
+            };
+        }
 
+        private static List<int> CreateRollsSequence()
+        {
+            return new List<int>
+            {
+                6,6,6,1,3,1,6,4,1,2,4,6,1,2,3,4,2,3,1,1,6,1,6,6,3,
+                4,6,4,4,2,6,4,2,5,5,4,6,4,1,6,5,5,5,5,1,2,3,3,4,4
+            };
+        }
     }
 
 }
